a34d70df9e4a31c4fac2c98f73190f214757526b
robsonsilv4 <robsonsilv410@gmail.com>
2019-05-02 04:37:02 -0300

    Criado usuário e ajustes no cadastro

--- app/src/main/java/com/robson/meucontador/SignUpActivity.java
+++ app/src/main/java/com/robson/meucontador/SignUpActivity.java
@@ -2,13 +2,41 @@ package com.robson.meucontador;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private EditText name;
+    private EditText username;
+    private EditText phone;
+    private EditText password1;
+    private EditText password2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
         getSupportActionBar().setTitle("Cadastro de Usuários");
+
+        name = findViewById(R.id.edit_text_name);
+        username = findViewById(R.id.edit_text_username);
+        phone = findViewById(R.id.edit_text_phone);
+        password1 = findViewById(R.id.edit_text_password1);
+        password2 = findViewById(R.id.edit_text_password2);
+    }
+
+    public void save(View view) {
+        User user = new User();
+        user.setName(name.getText().toString());
+        user.setName(username.getText().toString());
+        user.setName(phone.getText().toString());
+
+        if (password1.getText().toString().equals(password2.getText().toString())) {
+            Toast.makeText(this, "As senha não coincidem!", Toast.LENGTH_SHORT).show();
+        }
+
+        user.setPassword(password1.getText().toString());
     }
 }
---
+++ app/src/main/java/com/robson/meucontador/User.java
@@ -0,0 +1,43 @@
+package com.robson.meucontador;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+    private String name;
+    private String username;
+    private String phone;
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
