b3bf04f3ffd59256423b9aecc4a8034ea4282c34
robsonsilv4 <robsonsilv410@gmail.com>
2019-05-02 02:52:34 -0300

    Adicionado splash screen

---
+++ app/src/main/java/com/robson/meucontador/SignUpActivity.java
@@ -0,0 +1,14 @@
+package com.robson.meucontador;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        getSupportActionBar().setTitle("Cadastro de Usuários");
+    }
+}
---
+++ app/src/main/java/com/robson/meucontador/SplashScreenActivity.java
@@ -0,0 +1,35 @@
+package com.robson.meucontador;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private ImageView logo;
+    private static int splashTimeOut = 5000;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        logo = (ImageView) findViewById(R.id.logo);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, splashTimeOut);
+
+        Animation myanim = AnimationUtils.loadAnimation(this, R.anim.splash_screen_animation);
+        logo.startAnimation(myanim);
+    }
+}
